8b596e7187df7501e46f437953e6dc1e5bd8370b
John K.E. Wilson <wilsonfiifi@gmail.com>
2017-01-14 23:41:58 +0000

    moved database config to Client constructor

--- jsondb.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// error_reporting(E_ALL);
-
-// Helper functions
-require_once __DIR__ . '/src/helpers.php';
-
-// Setup configuration
-IBT\JsonDB\Client::$config = include_once(__DIR__ . '/config/config.php');
--- tests/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Autoload
-require_once __DIR__ . '/../vendor/autoload.php';
-
-// bootstrap
-require_once __DIR__ . '/../jsondb.php';
